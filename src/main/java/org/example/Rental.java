6d00ff9c1c283fc193eff7149527f6070b6dd7b6
Ivan Fernandez <ivanfl@ext.zara.com>
2023-11-09 10:50:06 +0100

    Moved methods to classes

--- src/main/java/org/example/Rental.java
+++ src/main/java/org/example/Rental.java
@@ -7,6 +7,37 @@ public class Rental
     this.daysRented = daysRented;
   }
 
+  int getFrequentRenterPoints() {
+    int frequentRenterPoints = 0;
+    if (getMovie().getPriceCode() == Movie.NEW_RELEASE
+        && getDaysRented() > 1) {
+      return frequentRenterPoints + 2;
+    }
+    return frequentRenterPoints + 1;
+  }
+
+  double calculateAmountForEachLine() {
+    double thisAmount = 0;
+    switch (getMovie().getPriceCode()) {
+      case Movie.REGULAR:
+        thisAmount += 2;
+        if (getDaysRented() > 2) {
+          thisAmount += (getDaysRented() - 2) * 1.5;
+        }
+        break;
+      case Movie.NEW_RELEASE:
+        thisAmount += getDaysRented() * 3;
+        break;
+      case Movie.CHILDRENS:
+        thisAmount += 1.5;
+        if (getDaysRented() > 3) {
+          thisAmount += (getDaysRented() - 3) * 1.5;
+        }
+        break;
+    }
+    return thisAmount;
+  }
+
   public int getDaysRented () {
     return daysRented;
   }
